1b8d48efe09420f5fe991b99b67b9b53ba4c617d
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2022-04-22 17:09:01 +0300

    before to db persistance

--- src/controller/commentController.php
+++ src/controller/commentController.php
@@ -2,11 +2,21 @@
 // symphony oldugu icin namespace e ihtiyac var, autoloader icin 
 namespace Mybasicmodule\Controller;
 
+use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Response;
 
-class CommentController {
+class CommentController extends FrameworkBundleAdminController{
     public function indexAction(){
         //Action i prefix yapmak zorundayiz symphony den dolayi
-        return new Response("Hello world");
+        // return new Response("Hello world");
+        //it uses twig as rendering template, to use twig we need to extends FrameWorkBundleAdminController
+        $form = $this->createFormBuilder()->add('name',TextType::class)->getForm();
+        return $this->render("@Modules/mybasicmodule/views/templates/admin/comment.html.twig",[
+            'test' => 123,
+            'form' => $form->createView()
+        ]);
+        // 2nd arguments are variable to pass twig
+
     }
 }
